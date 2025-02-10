16f3462b6afabd4662d780bf04b3965e5f45f17b
TangDuYen <YenTDSE171310@fpt.edu.vn>
2025-02-11 02:30:52 +0700

    update header & footer & slider

--- unityhands_fe/src/App.jsx
+++ unityhands_fe/src/App.jsx
@@ -2,10 +2,12 @@ import { Routes, Route } from "react-router-dom";
 import { PATH_NAME } from "./constant/pathname";
 import Login from "./pages/admin/Login/Login";
 import Register from "./pages/admin/Register/Register";
+import Homepage from "./pages/homepage/Homepage";
 const App = () => {
   return (
     <div>
       <Routes>
+        {/* <Route path={PATH_NAME.HOMEPAGE} element={<Homepage />} /> */}
         <Route path={PATH_NAME.LOGIN} element={<Login />} />
         <Route path={PATH_NAME.REGISTER} element={<Register />} />
       </Routes>
--- unityhands_fe/src/pages/homepage/Homepage.jsx
+++ unityhands_fe/src/pages/homepage/Homepage.jsx
@@ -1,18 +1,20 @@
-import Area from '../../components/area/Area';
-import Introduction from '../../components/introduction/Introduction';
-import './Homepage.scss';
-import Header from '../../components/header/Header';
+import Area from "../../components/area/Area";
+import Introduction from "../../components/introduction/Introduction";
+import "./Homepage.scss";
+import Header from "../../components/header/Header";
+import Footer from "../../components/footer/Footer";
 
 function Homepage() {
   return (
     <>
-    <Header />
-    <Introduction />
-    <Area />
-    <div className="homepage">
-      <h1 className="homepage-header">Welcome to My Homepage!</h1>
-      <button className="homepage-button">Click Me</button>
-    </div>
+      <header>
+        <Header />
+      </header>
+        <Introduction />
+        <Area />
+      <footer>
+        <Footer />
+      </footer>
     </>
   );
 }
