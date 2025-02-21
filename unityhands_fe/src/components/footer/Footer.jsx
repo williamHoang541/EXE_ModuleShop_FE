5930ca37e603e1ee405193fc99b3774bcc1636bd
VinhPV <vinhpvse172511@fpt.edu.vn>
2025-02-22 02:52:06 +0700

    update footer

--- unityhands_fe/src/components/footer/Footer.jsx
+++ unityhands_fe/src/components/footer/Footer.jsx
@@ -1,53 +1,59 @@
 import React from "react";
 import logo from "../../assets/logo.png";
 import { Link } from "react-router-dom";
-import { Button, ConfigProvider, Flex } from "antd";
-import "./Footer.css"
+import { FaMap, FaPhoneAlt } from "react-icons/fa";
+import { IoMdMail } from "react-icons/io";
+import "./Footer.css";
 
 function Footer() {
   return (
     <footer className="footer">
-      <Flex justify="space-between" className="footer_111">
-        <Link to="/">
-          <img className="footer__logo" src={logo} />
-        </Link>
-        <div className="footer__item">
-          <h4 className="footer__item__title">Sản phẩm</h4>
-          <p className="footer__item__content ">Về chúng tôi</p>
-          <p className="footer__item__content ">Đặt hàng</p>
-          <p className="footer__item__content ">Dịch vụ</p>
+      <div className="footer_wrapper">
+        <div className="footer_container">
+          <h3>Thông tin liên hệ</h3>
+          <h4>Công ty cổ phần Module Furniture</h4>
+          <div className="footer_item">
+            <FaMap />
+            <p className="footer_item_content">FPT University Hồ Chí Minh</p>
+          </div>
+          <div className="footer_item">
+            <FaPhoneAlt />
+            <p className="footer_item_content">1800.6789</p>
+          </div>
+          <div className="footer_item">
+            <IoMdMail />
+            <p className="footer_item_content">support@module.vn</p>
+          </div>
         </div>
-        <div className="footer__item">
-          <h4 className="footer__item__title">Chi nhánh</h4>
-          <p className="footer__item__content">CN1: 34 Đường C, Quận 1</p>
-          <p className="footer__item__content">CN2: 4 Đường B, Quận 2</p>
-          <p className="footer__item__content">CN3: 34 Đường H, Quận 3</p>
+
+        <div className="footer_container">
+          <h3>Chăm sóc khách hàng</h3>
+          <p className="footer_item_content">
+            Thời gian hỗ trợ 24/7 không kể ngày lễ
+          </p>
+          <h3>Thời gian hoạt động</h3>
+          <p className="footer_item_content">Thứ 2 - Thứ 6: 8:00 - 21:00</p>
+          <p className="footer_item_content">Thứ 7 - Chủ nhật: 8:00 - 16:00</p>
         </div>
-        <div className="footer__item">
-          <h4 className="footer__item__title">Thời gian hoạt động</h4>
-          <p className="footer__item__content">Thứ 2 - Thứ 6: 8:00 - 21:00</p>
-          <p className="footer__item__content">Thứ 7 - Chủ nhật: 8:00 - 16:00</p>
-          <ConfigProvider
-            theme={{
-              components: {
-                Button: {
-                  defaultColor: "white",
-                  defaultBg: "#43321b",
-                  defaultBorderColor: "#43321b",
-                  defaultHoverBorderColor: "black",
-                  defaultHoverColor: "white",
-                  defaultHoverBg: "#936a49",
-                  defaultActiveBg: "#43321b",
-                  defaultActiveBorderColor: "#43321b",
-                  defaultActiveColor: "black",
-                },
-              },
-            }}
-          >
-            <Button className="footer__item__button">Đặt hàng ngay</Button>
-          </ConfigProvider>
+
+        <div className="footer_container">
+          <h3>Hướng dẫn</h3>
+          <div className="footer_link_container">
+            <a href="#" className="footer_link">
+              Chính sách mua bán
+            </a>
+            <a href="#" className="footer_link">
+              Chính sách bảo mật
+            </a>
+            <a href="#" className="footer_link">
+              Quy định đối với người mua hàng
+            </a>
+            <a href="#" className="footer_link">
+              Hướng dẫn mua hàng
+            </a>
+          </div>
         </div>
-      </Flex>
+      </div>
     </footer>
   );
 }
