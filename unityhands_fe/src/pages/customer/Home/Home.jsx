06217da60b9a6b333283ecff886dd93d43a0126c
VinhPV <vinhpvse172511@fpt.edu.vn>
2025-02-24 01:32:49 +0700

    update home page

--- unityhands_fe/src/pages/customer/Home/Home.jsx
+++ unityhands_fe/src/pages/customer/Home/Home.jsx
@@ -1,5 +1,6 @@
 import "./Home.css";
 import { BsCart3 } from "react-icons/bs";
+import { FaArrowRightLong } from "react-icons/fa6";
 const Home = () => {
   return (
     <div className="home">
@@ -35,28 +36,111 @@ const Home = () => {
       <section className="home-section-2">
         <h3>Các sản phẩm nổi bật</h3>
         <div className="home-product-hot">
-        <div className="home-cart-product">
-        <div className="home-cart-img">
-          <img
-            src="https://bizweb.dktcdn.net/thumb/large/100/501/740/products/2-d053f572-d9ac-46c3-8505-6b5b9b783e83.jpg?v=1701329060077"
-            alt=""
-          />
-          </div>
-
-          <div className="home-cart-content">
-            <div className="home-cart-label">
-              Tủ quần áo 5 cánh hiện đại
+          <form className="home-cart-product">
+            <div className="home-cart-img">
+              <a href="#" className="home-cart-detail">
+                <img
+                  className="home-thumbnail"
+                  src="https://bizweb.dktcdn.net/thumb/large/100/501/740/products/2-d053f572-d9ac-46c3-8505-6b5b9b783e83.jpg?v=1701329060077"
+                  alt=""
+                />
+              </a>
             </div>
-          <div className="home-cart-coin">10.000.000₫</div>
-          </div>
 
-          <div className="home-cart-btn">
-            <button className="home-add-cart" type="submit">
-              <BsCart3 /> Thêm giỏ hàng
-            </button>
-          </div>
+            <div className="home-cart-content">
+              <div className="home-cart-label">Tủ quần áo 5 cánh hiện đại</div>
+              <div className="home-cart-coin">10.000.000₫</div>
+              <div className="home-cart-btn">
+                <button className="home-add-cart" type="submit">
+                  <BsCart3 className="home-icons" /> Thêm giỏ hàng
+                </button>
+              </div>
+            </div>
+          </form>
 
+          <form className="home-cart-product">
+            <div className="home-cart-img">
+              <a href="#" className="home-cart-detail">
+                <img
+                  className="home-thumbnail"
+                  src="https://bizweb.dktcdn.net/thumb/large/100/501/740/products/2-d053f572-d9ac-46c3-8505-6b5b9b783e83.jpg?v=1701329060077"
+                  alt=""
+                />
+              </a>
+            </div>
+
+            <div className="home-cart-content">
+              <div className="home-cart-label">Tủ quần áo 5 cánh hiện đại</div>
+              <div className="home-cart-coin">10.000.000₫</div>
+              <div className="home-cart-btn">
+                <button className="home-add-cart" type="submit">
+                  <BsCart3 className="home-icons" /> Thêm giỏ hàng
+                </button>
+              </div>
+            </div>
+          </form>
+
+          <form className="home-cart-product">
+            <div className="home-cart-img">
+              <a href="#" className="home-cart-detail">
+                <img
+                  className="home-thumbnail"
+                  src="https://bizweb.dktcdn.net/thumb/large/100/501/740/products/2-d053f572-d9ac-46c3-8505-6b5b9b783e83.jpg?v=1701329060077"
+                  alt=""
+                />
+              </a>
+            </div>
+
+            <div className="home-cart-content">
+              <div className="home-cart-label">Tủ quần áo 5 cánh hiện đại</div>
+              <div className="home-cart-coin">10.000.000₫</div>
+              <div className="home-cart-btn">
+                <button className="home-add-cart" type="submit">
+                  <BsCart3 className="home-icons" /> Thêm giỏ hàng
+                </button>
+              </div>
+            </div>
+          </form>
+
+          <form className="home-cart-product">
+            <div className="home-cart-img">
+              <a href="#" className="home-cart-detail">
+                <img
+                  className="home-thumbnail"
+                  src="https://bizweb.dktcdn.net/thumb/large/100/501/740/products/2-d053f572-d9ac-46c3-8505-6b5b9b783e83.jpg?v=1701329060077"
+                  alt=""
+                />
+              </a>
+            </div>
+
+            <div className="home-cart-content">
+              <div className="home-cart-label">Tủ quần áo 5 cánh hiện đại</div>
+              <div className="home-cart-coin">10.000.000₫</div>
+              <div className="home-cart-btn">
+                <button className="home-add-cart" type="submit">
+                  <BsCart3 className="home-icons" /> Thêm giỏ hàng
+                </button>
+              </div>
+            </div>
+          </form>
         </div>
+
+        <div className="home-read-more">
+          <a href="#">Xem thêm</a>
+          <FaArrowRightLong className="home-icon-arrow" />
+        </div>
+      </section>
+
+      <section className="home-section-3">
+        <div className="home-banner-container">
+          <a href="#">
+            <span className="home-banner-title">NGUỒN CẢM HỨNG VÔ TẬN</span>
+            <span className="home-banner-desc">
+              Khám phá nội thất thiết kế đương đại mang đến cảm giác thoải mái,
+              sang trọng. Cá nhân hoá trong từng sản phẩm phù hợp với mọi không
+              gian sống.
+            </span>
+          </a>
         </div>
       </section>
     </div>
